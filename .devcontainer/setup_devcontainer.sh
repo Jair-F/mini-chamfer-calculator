a86f8258497c3bcf5907b8487547272fd9492981
Jair-F <jair.fehlauer@gmail.com>
2025-04-04 20:51:27 +0000

    added dev container for project

---
+++ .devcontainer/setup_devcontainer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.name Jair-F
+git config --global user.email jair.fehlauer@gmail.com
+git config --global core.autocrlf true
+
+pip install -r requirements.txt
